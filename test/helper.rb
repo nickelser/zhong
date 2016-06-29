d48c13ffeb12d7a3937fe7ae2adc968cf26b4030
Nick Elser <nick.elser@gmail.com>
2016-06-29 14:00:30 -0700

    No fanciness in matching.

--- test/helper.rb
+++ test/helper.rb
@@ -2,7 +2,7 @@ $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 
 if ENV["CODECLIMATE_REPO_TOKEN"]
   require "codeclimate-test-reporter"
-  ::SimpleCov.add_filter "*helper*"
+  ::SimpleCov.add_filter "helper"
   CodeClimate::TestReporter.start
 end
 
